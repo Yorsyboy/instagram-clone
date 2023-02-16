6d62c435cb251084bcb2d212c992e46d29652143
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2023-02-16 18:42:58 +0100

    Use fake data using minifaker to create stories

---
+++ components/Stories.jsx
@@ -0,0 +1,26 @@
+import minifaker from 'minifaker';
+import 'minifaker/locales/en';
+import { useEffect, useState } from 'react';
+import Story from './Story';
+
+export default function  () {
+    const [storyUser, setStoryUser] = useState([]);
+
+    useEffect(() => {
+        const storyUsers = minifaker.array(20, (i) => ({
+            username: minifaker.username({ locale: 'en' }).toLocaleLowerCase(),
+            img: `https://i.pravatar.cc/150?img=${Math.ceil(Math.random() * 70)}`,
+            id: i,
+        }));
+        setStoryUser(storyUsers);
+        console.log(storyUsers)
+    }, []);
+
+  return (
+    <div>
+        {storyUser.map((user) => (
+            <Story key={user.id} username={user.username} img={user.img} />
+        ))}
+    </div>
+  )
+}
