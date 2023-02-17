8a1c3e6bbbeaee3a8c25b63e300047ee808889f3
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2023-02-17 20:19:52 +0100

    Modify mini profile

--- components/MiniProfile.jsx
+++ components/MiniProfile.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useSession, signOut } from "next-auth/react";
 
 export default function MiniProfile() {
+  const { data: session } = useSession();
   return (
-    <div className='flex items-center justify-between mt-14 ml-10'>
-        <img
-        className='h-16 w-16 rounded-full border p-[2px]'
-         src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" alt="User image" />
-        <div className='flex-1 ml-4'>
-            <h2 className='font-bold'>Username</h2>
-            <h3 className='text-sm text-gray-400'>Welcome to Instagram</h3>
-        </div>
-        <button className='font-semibold text-blue-400 text-s,'>Sign Out</button>
+    <div className="flex items-center justify-between mt-14 ml-10">
+      <img
+        className="h-16 w-16 rounded-full border p-[2px]"
+        src={session?.user.image}
+        alt="User image"
+      />
+      <div className="flex-1 ml-4">
+        <h2 className="font-bold">{session?.user.username}</h2>
+        <h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
+      </div>
+      <button onClick={signOut} className="font-semibold text-blue-400 text-s,">
+        Sign Out
+      </button>
     </div>
-  )
+  );
 }
--- components/Story.jsx
+++ components/Story.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
 
-export default function Story({ username, img }) {
+export default function Story({ username, img, isUser }) {
   return (
-    <div>
+    <div className="relative group cursor-pointer">
       <img
-        className="h-14 rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out"
+        className="h-14 rounded-full p-[1.5px] border-red-500 border-2 group-hover:scale-110 transition-transform duration-200 ease-out"
         src={img}
         alt="User image"
       />
+      {isUser && <AiOutlinePlus className="absolute top-5 left-5 text-white text-sm" />}
       <p className="text-xs w-14 truncate">{username}</p>
     </div>
   );
