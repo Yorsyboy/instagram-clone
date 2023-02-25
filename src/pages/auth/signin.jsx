8887621e036ee70990fd155a2c300dab37532024
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2023-02-25 21:34:52 +0100

    SignIn with google

--- src/pages/auth/signin.jsx
+++ src/pages/auth/signin.jsx
@@ -1,15 +1,21 @@
 import Header from "components/Header";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithRedirect,
+} from "firebase/auth";
 import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { db } from "../../../firebase";
 import { useRouter } from "next/router";
-
+import { FcGoogle } from "react-icons/fc";
 
 export default function Signin() {
   const router = useRouter();
+  const auth = getAuth();
   const onGoogleSignIn = async () => {
     try {
-      const auth = getAuth();
       const provider = new GoogleAuthProvider();
       await signInWithPopup(auth, provider);
       const user = auth.currentUser.providerData[0];
@@ -24,11 +30,35 @@ export default function Signin() {
           uid: user.uid,
           timeStamp: serverTimestamp(),
           username: user.displayName.split(" ").join("").toLocaleLowerCase(),
-        })
+        });
       }
       router.push("/");
     } catch (error) {
-      console.log(error)
+      console.log(error);
+    }
+  };
+
+  const onGithubSignIn = async () => {
+    try {
+      const provider = new GithubAuthProvider();
+      signInWithPopup(auth, provider);
+      const user = auth.currentUser.providerData[0];
+      //save user in database
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          userImg: user.photoURL,
+          uid: user.uid,
+          timeStamp: serverTimestamp(),
+          username: user.displayName.split(" ").join("").toLocaleLowerCase(),
+        });
+      }
+      router.push("/");
+    } catch (error) {
+      console.log(error);
     }
   };
   return (
@@ -56,18 +86,38 @@ export default function Signin() {
               >
                 Toyosi Taiwo{" "}
               </a>
-               for learning purpose
+              for learning purpose
             </p>
           </div>
-          <div className="flex flex-wrap">
-              <div className="">
-                <button
-                  className="bg-black inline-block m-1 px-6 py-2.5 mb-2 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out"
-                  onClick={onGoogleSignIn}
-                >
-                  Sign in with Google
-                </button>
-              </div>
+          <div className="">
+            <button
+              className="text-white bg-red-400 hover:bg-red-500 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 mr-2 mb-2"
+              onClick={onGoogleSignIn}
+            >
+              <FcGoogle className="w-4 h-4 mr-2 -ml-1" />
+              Sign in with Google
+            </button>
+            <button
+              className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
+              onClick={onGithubSignIn}
+            >
+              <svg
+                class="w-4 h-4 mr-2 -ml-1"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="github"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 496 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+                ></path>
+              </svg>
+              Sign in with Github
+            </button>
           </div>
         </div>
       </div>
